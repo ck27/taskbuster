e49c19a922862d48d8872e77e483187585a3ba4c
Chethan K <krishnamurthy.chethan@gmail.com>
2018-10-06 17:04:37 +0530

    Unit Test for Home Page

--- functional_tests/test_all_users.py
+++ functional_tests/test_all_users.py
@@ -5,15 +5,14 @@ import unittest
 class NewVisitorTest(unittest.TestCase):
  
     def setUp(self):
-        self.browser = webdriver.Chrome(executable_path="chromedriver")
-        self.browser.implicitly_wait(3)
- 
+        self.browser = webdriver.Chrome(executable_path="/usr/lib/chromium-browser/chromedriver")
+        
     def tearDown(self):
         self.browser.quit()
  
     def test_it_worked(self):
-        self.browser.get('http://localhost:8000')
-        self.assertIn('Django: the Web framework for perfectionists with deadlines.', self.browser.title)
+        self.browser.get('http://127.0.0.1:8000')
+        self.assertIn('Django Taskbuster', self.browser.title)
  
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
